1bcb45f6cdca85013bd4d9ebdc6a6c606071f52f
J-F David <jdavid@nuglif.com>
2016-12-09 13:22:19 -0500

    Add comment to existing JIRA ticket when PagerDuty alert is resolved.

--- web/pd2jira.php
+++ web/pd2jira.php
@@ -1,6 +1,5 @@
 <?php
 $messages = json_decode(file_get_contents("php://input"));
-
 $jira_url = getenv('JIRA_URL');
 if (substr($jira_url, strlen($jira_url)-1, 1) == "/") {
   $jira_url = substr($jira_url, 0, strlen($jira_url)-1);
@@ -16,7 +15,8 @@ if ($messages) foreach ($messages->messages as $webhook) {
   $webhook_type = $webhook->type;
 
   switch ($webhook_type) {
-    case "incident.trigger":
+    case "incident.acknowledge":
+	file_put_contents("php://stdout", "acknowledge!\n");
       if(file_exists('lock.txt') && file_get_contents('lock.txt') > (time() - 5)){
         die('Should not run!');
       }
@@ -26,7 +26,9 @@ if ($messages) foreach ($messages->messages as $webhook) {
       $ticket_url = $webhook->data->incident->html_url;
       $pd_requester_id = $webhook->data->incident->assigned_to_user->id;
       $service_name = $webhook->data->incident->service->name;
+      $service_name_label = str_replace(' ', '', $service_name);
       $assignee = $webhook->data->incident->assigned_to_user->name;
+      //$assigneeEmail = $webhook->data->incident->assigned_to_user->email;
       if ($webhook->data->incident->trigger_summary_data->subject) {
         $trigger_summary_data = $webhook->data->incident->trigger_summary_data->subject;
       }
@@ -34,10 +36,10 @@ if ($messages) foreach ($messages->messages as $webhook) {
         $trigger_summary_data = $webhook->data->incident->trigger_summary_data->description;
       }
 
-      $summary = "PagerDuty Service: $service_name, Incident #$incident_number, Summary: $trigger_summary_data";
+      $summary = "$trigger_summary_data";
       $summary = (strlen($summary) > 255) ? substr($summary,0,252) . '...' : $summary;
 
-      $verb = "triggered";
+      $verb = "acknowldged";
 
       //Let's make sure the note wasn't already added (Prevents a 2nd Jira ticket in the event the first request takes long enough to not succeed according to PagerDuty)
       $url = "https://$pd_subdomain.pagerduty.com/api/v1/incidents/$incident_id/notes";
@@ -53,11 +55,10 @@ if ($messages) foreach ($messages->messages as $webhook) {
           }
         }
       }
-
       //Create the JIRA ticket when an incident has been triggered
       $url = "$jira_url/rest/api/2/issue/";
 
-      $data = array('fields'=>array('project'=>array('key'=>"$jira_project"),'summary'=>"$summary",'description'=>"A new PagerDuty ticket has been created.  {$trigger_summary_data}. Please go to $ticket_url to view it.", 'issuetype'=>array('name'=>"$jira_issue_type")));
+      $data = array('fields'=>array('project'=>array('key'=>"$jira_project"),'summary'=>"$summary",'description'=>"*Une alerte PagerDuty a été _acknowledged_ par {$assignee}.* \n{$trigger_summary_data}. \nVous pouvez voir les détails de l'alerte à l'adresse suivante : $ticket_url", 'issuetype'=>array('name'=>"$jira_issue_type"), 'labels'=>array("pagerduty", "$service_name_label"), 'priority'=>array('name'=>"Critical"), 'assignee'=>array('name'=>"$assignee")));
       $data_json = json_encode($data);
 
       $return = http_request($url, $data_json, "POST", "basic", $jira_username, $jira_password);
@@ -73,6 +74,77 @@ if ($messages) foreach ($messages->messages as $webhook) {
         $data_json = json_encode($data);
         http_request($url, $data_json, "POST", "token", "", $pd_api_token);
       }
+    case "incident.resolve":
+	file_put_contents("php://stdout", "resolve!\n");
+       if(file_exists('lock.txt') && file_get_contents('lock.txt') > (time() - 5)){
+        die('Should not run!');
+      }
+      file_put_contents('lock.txt', time());
+      $incident_id = $webhook->data->incident->id;
+      $incident_number = $webhook->data->incident->incident_number;
+      $ticket_url = $webhook->data->incident->html_url;
+      $pd_requester_id = $webhook->data->incident->assigned_to_user->id;
+      $service_name = $webhook->data->incident->service->name;
+      $service_name_label = str_replace(' ', '', $service_name);
+      $resolvedBy = $webhook->data->incident->resolved_by_user->name;
+      $jiraTicket = "IN-";
+      //$assigneeEmail = $webhook->data->incident->assigned_to_user->email;
+      if ($webhook->data->incident->trigger_summary_data->subject) {
+        $trigger_summary_data = $webhook->data->incident->trigger_summary_data->subject;
+      }
+      else {
+        $trigger_summary_data = $webhook->data->incident->trigger_summary_data->description;
+      }
+
+      $summary = "$trigger_summary_data";
+      $summary = (strlen($summary) > 255) ? substr($summary,0,252) . '...' : $summary;
+
+      $verb = "resolved";
+		file_put_contents("php://stdout", $resolvedBy . "\n");
+      //Il faut trouver le numéro de billet JIRA. 
+      file_put_contents("php://stdout", "cherche billet jira!\n");
+      $url = "https://$pd_subdomain.pagerduty.com/api/v1/incidents/$incident_id/notes";
+      $return = http_request($url, "", "GET", "token", "", $pd_api_token);
+      if ($return['status_code'] == '200') {
+        $response = json_decode($return['response'], true);
+        if (array_key_exists("notes", $response)) {
+          foreach ($response['notes'] as $value) {
+            $startsWith = "JIRA ticket";
+            $theNote = $value['content'];
+            
+            if (substr($theNote, 0, strlen($startsWith)) === $startsWith) {
+         		//$jiraTicket = find_between($theNote, $startsWith, 'has');
+         		preg_match("/(?<=ticket).*?(?=has)/s", $theNote, $result);
+         		$jiraTicket = $result[0];
+         		$jiraTicket = str_replace(' ', '', $jiraTicket);
+            }
+            file_put_contents("php://stdout", $jiraTicket);
+          }
+        }
+      }
+      //Ajout d'un commentaire au billet JIRA trouvé précédemment. 
+      $url = "$jira_url/rest/api/2/issue/$jiraTicket";
+	
+	if($resolvedBy == '') {
+      $data = array('update'=>array('comment'=>array(array('add'=>array('body'=>"L'alerte PagerDuty s'est résolue par elle-même.")))));
+    }
+    else {
+    $data = array('update'=>array('comment'=>array(array('add'=>array('body'=>"L'alerte PagerDuty s'est résolue par {$resolvedBy}.")))));
+	}
+	$data_json = json_encode($data);
+      $return = http_request($url, $data_json, "PUT", "basic", $jira_username, $jira_password);
+      $status_code = $return['status_code'];
+      $response = $return['response'];
+      $response_obj = json_decode($response);
+      $response_key = $response_obj->key;
+
+      if ($status_code == "201") {
+        //Update the PagerDuty ticket with the JIRA ticket information.
+        $url = "https://$pd_subdomain.pagerduty.com/api/v1/incidents/$incident_id/notes";
+        $data = array('note'=>array('content'=>"JIRA ticket $response_key has been updated.  You can view it at $jira_url/browse/$response_key."),'requester_id'=>"$pd_requester_id");
+        $data_json = json_encode($data);
+        http_request($url, $data_json, "POST", "token", "", $pd_api_token);
+      }
       else {
         //Log the error
         error_log("Error: Returned status code " . $status_code . " with response " . $response);
@@ -82,8 +154,8 @@ if ($messages) foreach ($messages->messages as $webhook) {
         $data_json = json_encode($data);
         http_request($url, $data_json, "POST", "token", "", $pd_api_token);
       }
-      break;
     default:
+    file_put_contents("php://stdout", "default!\n");
       continue;
   }
 }
